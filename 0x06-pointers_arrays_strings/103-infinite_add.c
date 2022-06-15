c82935c327ea12209f05d63273a9122d4567a78b
KingLexy <82228858+KingLexy@users.noreply.github.com>
2022-06-15 20:36:52 +0100

    Hot fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,61 +1,59 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * infinite_add - Function prototype
- * Description: Adds two numbers
- * @n1: First number
- * @n2: Second number
- * @r: The buffer to store the result
- * @size_r: The size of the buffer r
- * Return: Pointer to the result
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int main(void)
 {
-	int a, i, x = 0, y = 0, t = 0;
+        char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
+        char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
+        char r[100];
+        char r2[10];
+        char r3[11];
+        char *res;
 
-	for (i = 0; *(n1 + i); i++)
-		x++;
-
-	for (i = 0; *(n2 + i); i++)
-		y++;
-
-	if (size_r <= x + 1 || size_r <= y + 1)
-		return (0);
-
-	n1 += x - 1, n2 += y - 1;
-
-	*(r + size_r) = '\0';
-
-	for (; *n1 && *n2; n1--, n2--, size_r--)
-	{
-		a = (*n1 - '0') + (*n2 - '0');
-		a += t;
-		*(r + size_r) = (a % 10) + '0';
-		t = a / 10;
-	}
-
-	for (; *n1; n1--, size_r--)
-	{
-		a = (*n1 - '0') + t;
-		*(r + size_r) = (a % 10) + '0';
-		t = a / 10;
-	}
-
-	for (; *n2; n2--, size_r--)
-	{
-		a = (*n2 - '0') + t;
-		*(r + size_r) = (a % 10) + '0';
-		t = a / 10;
-	}
-
-	if (t && size_r >= 0)
-	{
-		*(r + size_r) = (t % 10) + '0';
-		return (r + size_r);
-	}
-
-	if (t && size_r < 0)
-		return (0);
-
-	return (r + size_r + 1);
+        res = infinite_add(n, m, r, 100);
+        if (res == 0)
+        	{
+                printf("Error\n");
+        	}
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        n = "1234567890";
+        m = "1";
+        res = infinite_add(n, m, r2, 10);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        n = "999999999";
+        m = "1";
+        res = infinite_add(n, m, r2, 10);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        res = infinite_add(n, m, r3, 11);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        return (0);
 }
